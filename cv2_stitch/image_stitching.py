cbb4b9536c5c7baa8003ac8619109554d1e03f7b
Anh Phan <anhphanvt2705@gmail.com>
2023-07-06 12:04:54 +0700

    developing crop functions

--- cv2_stitch/image_stitching.py
+++ cv2_stitch/image_stitching.py
@@ -45,71 +45,67 @@ def get_threshold_image(gray_image):
     print("[CONSOLE] Thresholding image")
     return cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY)[1]
 
-def get_countour_dim(gray_image):
-    contours = cv2.findContours(gray_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(contours)
-    max_contours = max(contours, key=cv2.contourArea)
-    return cv2.boundingRect(max_contours)
-
-def get_mask_image(gray_image):
-    mask = np.zeros(gray_image.shape, dtype="uint8")
-    (x, y, w, h) = get_countour_dim(gray_image)
-    cv2.rectangle(mask, (x, y), (x + w, y + h), 255, -1)
-    return mask
+# def get_countour_dim(gray_image):
+#     contours = cv2.findContours(gray_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#     contours = imutils.grab_contours(contours)
+#     max_contours = max(contours, key=cv2.contourArea)
+#     return cv2.boundingRect(max_contours)
 
 def get_image_2D_dim(image):
     return image.shape[:2]
 
-def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
+def crop_image(image, factor):
+    (h, w) = get_image_2D_dim(image)
+    # Crop horizontally (width)
+    amount_crop = w * (1-factor)
+    w_upper = int(w - amount_crop//2)
+    w_lower = int(amount_crop//2)
+    # Crop vertically (height)
+    amount_crop = h * (1-factor)
+    h_upper = int(h - amount_crop//2)
+    h_lower = int(amount_crop//2)
+    return image[h_lower:h_upper, w_lower:w_upper]
 
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
-
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation = inter)
-
-    # return the resized image
-    return resized
+def is_black_pixel_outline(threshold_image):
+    # Find if there is black pixel on 4 sides
+    (height, width) = get_image_2D_dim(threshold_image)
+    # Lower side (0, w)
+    for w in range(0, width):
+        if all(threshold_image[0, w] == [0, 0]):
+            print(0, w)
+            return True
+    # Upper side (h, w)
+    for w in range(0, width):
+        if all(threshold_image[height-1, w] == [0, 0]):
+            print(height-1, w)
+            return True
+    # Left side (h, 0)
+    for h in range(0, height):
+        if all(threshold_image[h, 0] == [0, 0, 0]):
+            print(h, 0)
+            return True
+    # Right side (h, w)
+    for h in range(0, height):
+        if all(threshold_image[h, width-1] == [0, 0, 0]):
+            print(h, width-1)
+    return False
     
-
 images = get_images("./images/real/*.jpg")
 stitched_image = get_stitch_image(images)
 gray_stitched_image = get_gray_image(stitched_image)
-thresdold_image = get_threshold_image(gray_stitched_image)
-mask_image = get_mask_image(thresdold_image)
+threshold_image = get_threshold_image(gray_stitched_image)
+threshold_image_b = cv2.GaussianBlur(threshold_image, (3, 3), 0)
+threshold_image = get_threshold_image(threshold_image_b)
 
-# # Cropping
-# print("[CONSOLE] Cropping Image")
-# if stitched_image.shape[:2] == mask_image.shape[:2]:
-#     (stitch_h, stitch_w) = get_image_2D_dim(stitched_image)
-#     (mask_h, mask_w) = get_image_2D_dim(mask_image)
+# Cropping
+print("[CONSOLE] Cropping Image")
+threshold_image = crop_image(threshold_image, 0.815)
+if is_black_pixel_outline(threshold_image):
+    print("still black")
+else:
+    print("ya good")
+
     
-#     for h in range(mask_h, mask_h//2, -1):
-#         resized_mask = image_resize(mask_image, height=h)
-    
-# else:
-#     raise Exception("Starting mask and image does not equal")
-    
-show_image("hello", stitched_image)
-# write_image("./output/thresh_img.jpg", thresdold_image)
+show_image("hello", threshold_image)
+write_image("./output/test_img.jpg", threshold_image)
 
